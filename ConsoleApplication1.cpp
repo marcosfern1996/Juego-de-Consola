57e26a5e542e86215cd4115ab90be5884f45cada
marcosfern1996 <81453884+marcosfern1996@users.noreply.github.com>
2021-06-26 23:50:21 -0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include "rlutil.h"
+#include "funcionesJuego.h"
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+using namespace rlutil;
+
+int main()
+{
+	srand(time(NULL));
+	setBackgroundColor(WHITE);
+	mmenu();
+	system("pause");
+
+}
+
+
+
+
+
+
+
+
+
